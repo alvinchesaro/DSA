2d657e972394ce53713c0028b209a9977b62d718
alvinchesaro <alvinchesaro@outlook.com>
2025-03-31 20:01:16 +0300

    Initial

--- RemoveDuplicatesFromSortedArray/Program.cs
+++ RemoveDuplicatesFromSortedArray/Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+   
 
+    public static int RemoveDuplicates(int[] nums)
+    {
+        int dup = 0;
+        List<int> distinctList = new List<int>();
+
+        foreach (int num in nums)
+        {
+            if (!distinctList.Contains(num))
+            {
+                distinctList.Add(num);
+            }
+        }
+        
+
+        return dup;
+    }
+
+    static void Main()
+    {
+        int[] nums = { 1, 2, 2, 3,3,3, 4, 6, 8, 9 };
+        RemoveDuplicates(nums);
+
+        Console.ReadLine();
+    }
+}
